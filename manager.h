53a728a066e25220c89921b0750723db84acb6cd
Hamza Yusuff <hbyusuff@uwaterloo.ca>
2021-08-07 10:59:40 -0400

    added manager.h

---
+++ manager.h
@@ -0,0 +1,24 @@
+#ifndef _MANAGER_H
+#define _MANAGER_H
+// #include <iostream>
+// #include <sstream>
+// #include <stdlib.h>
+// #include <time.h>
+// #include <string>
+// #include <vector>
+// #include <memory>
+#include "grid.h"
+#include "block.h"
+#include "textdisplay.h"
+#include "commandmanager.h"
+#include "Level.h"
+
+class Manager{
+    Grid *theGrid;
+    CommandManager *ComManage;
+    TextDisplay * theTextDisplay;
+    public:
+        void play(int argc, char * argv[]);
+};
+
+#endif
