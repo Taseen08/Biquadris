51ed7afc9ab3e34a00b12d6b5d30ee88e4974e4d
Hamza Yusuff <hbyusuff@uwaterloo.ca>
2021-08-07 15:27:13 -0400

    added valid

--- Operations.cc
+++ Operations.cc
@@ -26,3 +26,62 @@ void Operations::moveBlock(Grid &theGrid, int x, int y, int rotation){
     theGrid.current->setRotate(degree);
     
 }
+
+
+bool Operations::isValid(Grid &theGrid, int x, int y, int rotation){
+
+    // edges
+
+    if (x==0 && y==0 && rotation==0){
+        for (int r=0; r<4; ++r){
+            for (int c=0; c<4; ++c){
+                int bY = theGrid.current->getY()+r;
+                int bX = theGrid.current->getX()+c;
+                char value = theGrid.current->getBlock()[theGrid.current->getRotate()][r][c];
+                if (value!='.'){
+                    if (bY <0 || bY >= theGrid.height || 
+                    bX < 0 || bX>=theGrid.width){
+                        return false;
+                    }
+                    if (theGrid.mainGrid[bY][bX].isOccupied()){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    int rotate = (theGrid.current->getRotate()+rotation)%4;
+
+    for (int r=0; r<4; ++r){
+        for (int c=0; c<4; ++c){
+            char value = theGrid.current->getBlock()[rotate][r][c];
+            int bY = theGrid.current->getY() + y +r;
+            int bX = theGrid.current->getX() + x + c;
+
+            if (value != '.'){
+
+                if (bY < 0 || bY >= theGrid.height || 
+                bX < 0 || bX >= theGrid.width){
+                    return false;
+                }
+
+                if (theGrid.mainGrid[bY][bX].isOccupied()){
+
+                    if (bY < theGrid.current->getY() ||
+                    bY > theGrid.current->getY()+3 ||
+                    bX < theGrid.current->getX() ||
+                    bX > theGrid.current->getX()+3){
+                        return false;
+                    }
+
+                    if (theGrid.current->getBlock()[theGrid.current->getRotate()][r+y][c+x] == '.'){
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
